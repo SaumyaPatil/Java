0b21d2d122ac6ceb26005adc34b3f05755d0494d
SaumyaPatil <saupatil10dec@gmail.com>
2025-05-27 18:00:30 +0530

    First Java code

---
+++ src/A.java
@@ -0,0 +1,8 @@
+class A{
+    public static void main(String []a){
+
+        //System is a pre-defined class while print is a pre-defined method used to print any text message on the output screen.
+        System.out.print("Welcome Java");
+    }
+}
+
