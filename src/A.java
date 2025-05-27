770796b1fd30a34220c72b87d55920904933e7a4
SaumyaPatil <saupatil10dec@gmail.com>
2025-05-28 00:30:42 +0530

    Static keyword and objects in java

--- src/A.java
+++ src/A.java
@@ -1,8 +1,13 @@
 class A{
+
+    static void show(){
+        System.out.println("Hello");
+    }
+
     public static void main(String []a){
 
         //System is a pre-defined class while print is a pre-defined method used to print any text message on the output screen.
-        System.out.print("Welcome Java");
+        System.out.print(a[1]);
     }
 }
 
