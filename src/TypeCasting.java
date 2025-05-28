96c1536ad943339c00250cc0a0c27bdf8fe044a9
SaumyaPatil <saupatil10dec@gmail.com>
2025-05-28 16:19:06 +0530

    Performing input and output operation in java

--- src/TypeCasting.java
+++ src/TypeCasting.java
@@ -1,10 +1,15 @@
 class TypeCasting{
     public static void main(String args[]){
 
-        int a=10;      //4 bytes
-        double b=a;   //8 bytes
-        System.out.println(b);    //10.0
+        //Implicit typecasting
+//        int a=10;      //4 bytes
+//        double b=a;   //8 bytes
+//        System.out.println(b);    //10.0
 
+        //Explicit typecasting
+        double c=10.5;
+        int d=(int)c;
+        System.out.println(d);
     }
 }
 
