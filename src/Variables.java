e39d28342252a4dfe4ceb3d8f5764d45b4707c13
SaumyaPatil <saupatil10dec@gmail.com>
2025-05-28 16:07:58 +0530

    This was comparison between static and local variable in java

--- src/Variables.java
+++ src/Variables.java
@@ -1,14 +1,27 @@
 class Variables{
 
-    static int b=20; //Static
+    static int b=20; //Static  -> iska single memory banta hai aur iska lifetime poore program tak hota hai
+    //ye apni older value ko hold karke rakhta hai!
     int c = 10;     //Instance
+
+    void fun(){
+        int c=20;
+        System.out.println(b+" "+c);
+        ++b;
+        ++c;
+    }
+
     public static void main(String[] args) {
 
         int a=10;      //Local
-        A ref=new A();
+        Variables ref=new Variables();
         System.out.println(a);
-        System.out.println(A.b);
+        System.out.println(Variables.b);
         System.out.println(ref.c);
+        //Single copy of static variables while multiple copies of local variables
+        //Life span of static variables is till the program is completely executed
+        ref.fun(); //20 20
+        ref.fun(); //21 20
 
     }
 }
