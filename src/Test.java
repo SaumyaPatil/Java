4f27299365d89d002756adef420ed1c3ec8c8048
SaumyaPatil <saupatil10dec@gmail.com>
2025-05-28 13:15:56 +0530

    Learning about comments in JAVA

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+class Test{
+
+    int a = 10;          //instance variable (variable declared inside a class body but outside of any method
+    static int b = 20;   //static variable
+
+
+    public static void main(String[] args) {
+        int c = 40;    //local variable (variable inside a method)
+        final int d = 50;   //final variable (the value cannot be changed)
+    }
+}
+
