3aa1587e2a3e7f2c64b07de1aeb9315b44d7d91f
SaumyaPatil <saupatil10dec@gmail.com>
2025-05-28 16:10:18 +0530

    Lets learn implicit and explicit typecasting

---
+++ src/TypeCasting.java
@@ -0,0 +1,10 @@
+class TypeCasting{
+    public static void main(String args[]){
+
+        int a=10;      //4 bytes
+        double b=a;   //8 bytes
+        System.out.println(b);    //10.0
+
+    }
+}
+
