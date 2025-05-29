9f66b82c9a9cc7cb93fac7c55aed885c739ae56b
SaumyaPatil <saupatil10dec@gmail.com>
2025-05-29 14:28:18 +0530

    This is simple if statement

--- src/Bitwise.java
+++ src/Bitwise.java
@@ -1,5 +1,10 @@
 class Bitwise{
     public static void main(String[] args) {
 
+        int a=5,b=7;
+        System.out.println("Bitwise AND " + (a & b));
+        System.out.println("Bitwise OR " + (a | b));
+        System.out.println("Bitwise XOR " + (a ^ b));
+        System.out.println("Bitwise complement " + (~a));
     }
 }
---
+++ src/SimpleIf.java
@@ -0,0 +1,22 @@
+
+//Simple if statement
+
+import java.util.Scanner;
+
+class SimpleIf{
+    public static void main(String[] args) {
+
+        if age;
+        System.out.println("Enter your age ");
+        Scanner sc = new Scanner(System.in);
+        age = r.nextInt();
+        System.out.println(age);
+
+        if(age >= 18){
+            System.out.println("Eligible for votes ");
+        }
+
+        System.out.println("Thankyou!");
+    }
+}
+
