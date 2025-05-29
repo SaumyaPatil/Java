a2b37c7f5355cf4bb4b5bc4b67a7d2e06834aaf4
SaumyaPatil <saupatil10dec@gmail.com>
2025-05-29 13:18:01 +0530

    This was about logical operators

---
+++ src/Logical.java
@@ -0,0 +1,23 @@
+
+//Logical operator example!
+
+class Logical{
+    public static void main(String[] args) {
+        //You can perform the operations on values and variables both
+        System.out.println("Logical AND ");
+        System.out.println((10>5) && (2>1));    //true
+        System.out.println((10>5) && (2<1));    //false
+        System.out.println((10<5) && (2<1));    //false
+
+        System.out.println("Logical OR ");
+        System.out.println((10>5) && (2>1));    //true
+        System.out.println((10>5) && (2<1));    //true
+        System.out.println((10<5) && (2<1));    //false
+
+        System.out.println("Logical NOT ");
+        System.out.println(!(10>5));     //false
+        System.out.println(!(10<5));     //true
+
+
+    }
+}
