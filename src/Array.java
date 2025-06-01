aa8d176a8617e747625901d4a97abc7da5c5664e
SaumyaPatil <saupatil10dec@gmail.com>
2025-06-01 16:41:26 +0530

    Array code in java

---
+++ src/Array.java
@@ -0,0 +1,17 @@
+class Array{
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 30, d = 40, e = 50;
+
+        int a[] = {10, 20, 30, 40, 50};
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+
+
+        //Here we have just 5 students so we made 5 variables.
+        //If we need to store for 1000 students we cant create so many variables.
+        //To store these multiple variables we have array where we can directly assign variables
+    }
+}
