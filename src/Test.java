8c829c30b716974b52e61e79cc47249e46ce8ca7
SaumyaPatil <saupatil10dec@gmail.com>
2025-06-06 23:58:23 +0530

    Classes and objects in OOPs

--- src/Test.java
+++ src/Test.java
@@ -1,21 +1,25 @@
-class Test{
+class ObjectsAndClass{
 
-    int a = 10;          //instance variable (variable declared inside a class body but outside of any method
-    static int b = 20;   //static variable
+    int a=10;
+    String b="ankush";
 
-
-    public static void main(String[] args) {
-        int c = 40;    //local variable (variable inside a method)
-        final int D = 50;   //final variable (the value cannot be changed)
-
-        Test t = new Test();   //object creation (We cannot access the instance variable without creating object. t is the reference variable inside of which object reference is stored.)
-        System.out.println(t.a);
-        System.out.println(b);
-
-        System.out.println(c);
-        System.out.println(D);   //final variable ko represent krneka treeka hai ye capital letter se
-
+    void Show(){
+        System.out.println(a + " " + b);
     }
 }
 
-//Comments are used to make programme more readable for other programmers so that the code is well defined.
+class Test{
+
+    public static void main(String[] args) {
+
+        //This is a way to create object of a class
+        ObjectsAndClass obj = new ObjectsAndClass();
+        obj.Show();
+
+
+        //Way 2
+        ObjectsAndClass r;
+        r = new ObjectsAndClass();
+        r.Show();
+    }
+}
